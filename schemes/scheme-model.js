34a224d023f284ff2b4aa310369c8d8bc9214471
Babatunde <babatundea15@gmail.com>
2020-05-23 17:58:18 +0100

    I fixed endpoint for getting scheme by Id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,10 +1,16 @@
 const db = require("../data/dbConfig.js");
 module.exports={
-    find
+    find,
+    findById
 }
 
 function find() {
-    // This is the equivalent of SELECT * FROM Schemes
-    return db("schemes");
+// This is the equivalent of SELECT * FROM Schemes
+return db("schemes");
     
+}
+
+function findById(id) {
+    // This is the equivalent SELECT * FROM Schemes where id=id;
+    return db("schemes").where({id}).first();
 }
