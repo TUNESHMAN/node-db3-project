edf40ccb94a9d6c94c36e0ea8c86d660c4e8d901
Babatunde <babatundea15@gmail.com>
2020-05-23 18:59:50 +0100

    I fixed the helper function to add steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,16 +1,25 @@
 const db = require("../data/dbConfig.js");
-module.exports={
-    find,
-    findById
-}
+module.exports = {
+  find,
+  findById,
+  findSteps,
+};
 
 function find() {
-// This is the equivalent of SELECT * FROM Schemes
-return db("schemes");
-    
+  // This is the equivalent of SELECT * FROM Schemes
+  return db("schemes");
 }
 
 function findById(id) {
-    // This is the equivalent SELECT * FROM Schemes where id=id;
-    return db("schemes").where({id}).first();
+  // This is the equivalent SELECT * FROM Schemes where id=id;
+  return db("schemes").where({ id }).first();
+}
+
+function findSteps(scheme_id) {
+  // This is the equivalent of SELECT steps.instructions, schemes.scheme_name FROM STEPS JOIN Schemes ON steps.scheme_id = schemes.id
+  return db
+    .select("steps.instructions")
+    .from("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where({ scheme_id });
 }
